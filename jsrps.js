a05ae4378705f319474b5d896632f0aee5d9efc0
Willner30 <willmdj@hotmail.com>
2022-12-22 19:38:14 -0500

    Add files via upload

--- jsrps.js
+++ jsrps.js
@@ -1,12 +1,17 @@
-let trianglePlayer = document.getElementById("triangleImg2");
-let rockPlayer = document.getElementById("rockImg2");
-let paperPlayer = document.getElementById("paperImg2");
-let scissorsPlayer = document.getElementById("scissorsImg2");
-let triangleCPU = document.getElementById("triangleImg");
-let rockCPU = document.getElementById("rockImg");
-let paperCPU = document.getElementById("paperImg");
-let scissorsCPU = document.getElementById("scissorsImg");
+const trianglePlayer = document.getElementById("triangleImg2");
+const rockPlayer = document.getElementById("rockImg2");
+const paperPlayer = document.getElementById("paperImg2");
+const scissorsPlayer = document.getElementById("scissorsImg2");
+const triangleCPU = document.getElementById("triangleImg");
+const rockCPU = document.getElementById("rockImg");
+const paperCPU = document.getElementById("paperImg");
+const scissorsCPU = document.getElementById("scissorsImg");
+const result = document.getElementById("result");
+const cpuScore = document.getElementById("cpuScore");
+const playerScore = document.getElementById("playerScore");
 var cpuTurn = Math.floor(Math.random() * 3);
+var currentScorePlayer = 0;
+var currentScoreCPU = 0;
 
 function playerPlaysRock () {
     paperPlayer.style.display = 'none';
@@ -39,7 +44,30 @@ function playerPlaysScissors () {
 }
 
 function restart () {
-    window.location.reload();
+    trianglePlayer.style.display = 'block';
+    rockPlayer.style.display = 'block';
+    rockPlayer.style.transform = "scale(1)";
+    rockPlayer.style.transition = "transform 0.75s ease";
+    paperPlayer.style.display = 'block';
+    paperPlayer.style.transform = "scale(1)";
+    paperPlayer.style.transition = "transform 0.75s ease";
+    scissorsPlayer.style.display = 'block';
+    scissorsPlayer.style.transform = "scale(1)";
+    scissorsPlayer.style.transition = "transform 0.75s ease";
+    triangleCPU.style.display = 'block';
+    rockCPU.style.display = 'block';
+    rockCPU.style.transform = "scale(1)";
+    rockCPU.style.transition = "transform 0.75s ease";
+    paperCPU.style.display = 'block';
+    paperCPU.style.transform = "scale(1)";
+    paperCPU.style.transition = "transform 0.75s ease";
+    scissorsCPU.style.display = 'block';
+    scissorsCPU.style.transform = "scale(1)";
+    scissorsCPU.style.transition = "transform 0.75s ease";
+    result.innerHTML = ""; 
+    rockPlayer.style.pointerEvents = 'initial';
+    paperPlayer.style.pointerEvents = 'initial';
+    scissorsPlayer.style.pointerEvents = 'initial';
 }
 
 function cpuPlay () {
@@ -70,28 +98,56 @@ function cpuPlay () {
 
 }
 
+function drawMessage () {
+    result.style.transition = "transform 0.75s ease";
+    result.innerHTML = "It's a Draw!!";
+    cpuTurn = Math.floor(Math.random() * 3); 
+}
+
+function winMessage () {
+    result.style.transition = "transform 0.75s ease";
+    result.innerHTML = "You win!!"; 
+    currentScorePlayer++;
+    playerScore.innerHTML = currentScorePlayer; 
+    cpuTurn = Math.floor(Math.random() * 3);
+}
+
+function loseMessage () {
+    result.style.transition = "transform 0.75s ease";
+    result.innerHTML = "You lose :/"; 
+    currentScoreCPU++;
+    cpuScore.innerHTML = currentScoreCPU; 
+    cpuTurn = Math.floor(Math.random() * 3);
+}
+
 function whoWon () {
 
     if ((cpuTurn === 0) && (rockPlayer.style.display === 'block')){
-        alert("It's a Draw!!");
+        rockPlayer.style.pointerEvents = 'none';
+        drawMessage();
     } else if ((cpuTurn === 1) && (paperPlayer.style.display === 'block')){
-        alert("It's a Draw!!");
+        paperPlayer.style.pointerEvents = 'none';
+        drawMessage();
     } else if ((cpuTurn === 2) && (scissorsPlayer.style.display === 'block')){
-        alert("It's a Draw!!");
+        scissorsPlayer.style.pointerEvents = 'none';
+        drawMessage();
     } else if ((cpuTurn === 0) && (paperPlayer.style.display === 'block')){
-        alert("You win!!");
+        paperPlayer.style.pointerEvents = 'none';
+        winMessage();
     } else if ((cpuTurn === 0) && (scissorsPlayer.style.display === 'block')){
-        alert("You lose :/");
+        scissorsPlayer.style.pointerEvents = 'none';
+        loseMessage();
     } else if ((cpuTurn === 1) && (rockPlayer.style.display === 'block')){
-        alert("You lose :/");
+        rockPlayer.style.pointerEvents = 'none';
+        loseMessage();
     } else if ((cpuTurn === 1) && (scissorsPlayer.style.display === 'block')){
-        alert("You win!!");
+        scissorsPlayer.style.pointerEvents = 'none';
+        winMessage();
     } else if ((cpuTurn === 2) && (rockPlayer.style.display === 'block')){
-        alert("You win!!");
+        rockPlayer.style.pointerEvents = 'none';
+        winMessage();
     } else if ((cpuTurn === 2) && (paperPlayer.style.display === 'block')){
-        alert("You lose :/");
-    } else {
-        return;
+        paperPlayer.style.pointerEvents = 'none';
+        loseMessage();
     }
-
 }
